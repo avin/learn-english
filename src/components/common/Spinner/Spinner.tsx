d4c617a72e5865ccd1d45cafccf32e2b215bb37e
a.vinogradov <a.vinogradov@bpcbt.com>
2024-04-16 00:42:55 +0300

    upd

--- src/components/common/Spinner/Spinner.tsx
+++ src/components/common/Spinner/Spinner.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 import cn from 'clsx';
 import styles from './Spinner.module.css';
 
@@ -10,10 +10,40 @@ interface Props {
 }
 
 const Spinner = ({ size = 80, color, className, thickness }: Props): JSX.Element => {
+  const elRef = useRef<HTMLDivElement>(null);
+  const frameId = useRef<number | null>(null);
+
+  useLayoutEffect(() => {
+    const duration = 700;
+
+    const rotateSpinner = () => {
+      const currentTime = Date.now();
+      const elapsedTime = currentTime;
+      const rotation = ((elapsedTime / duration) * 360) % 360;
+      if (elRef.current) {
+        elRef.current.style.transform = `rotate(${rotation}deg)`;
+      }
+      frameId.current = requestAnimationFrame(rotateSpinner);
+    };
+    frameId.current = requestAnimationFrame(rotateSpinner);
+
+    return () => {
+      if (frameId.current) {
+        cancelAnimationFrame(frameId.current);
+      }
+    };
+  }, []);
+
   return (
     <div
       className={cn(styles.spinner, className)}
-      style={{ width: size, height: size, color, borderWidth: thickness }}
+      ref={elRef}
+      style={{
+        width: size,
+        height: size,
+        color,
+        borderWidth: thickness,
+      }}
     />
   );
 };
